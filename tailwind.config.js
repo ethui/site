04800561f2fe980761bf8799013ca56fd87a9faa
Miguel Palhas <mpalhas@gmail.com>
2025-02-12 17:19:06 +0000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import baseConfig from "@ethui/ui/tailwind.config";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ["node_modules/@ethui/ui/components/**/*.js", ".app/**/*.tsx"],
+  content: ["node_modules/@ethui/ui/components/**/*.js", "app/**/*.tsx"],
   presets: [baseConfig],
   plugins: [require("tailwindcss-animate")],
 };
