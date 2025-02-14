4213ae99c0ea2efea6f6d8b75b10a0676ad21d9e
Miguel Palhas <mpalhas@gmail.com>
2025-02-14 15:28:38 +0000

    wip

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,6 +16,34 @@ interface Data {
   linux: any;
 }
 
+const features = [
+  {
+    title: "Local-first",
+    description:
+      "works directly with anvil and forge to provide a fast and reproducible environment",
+  },
+  {
+    title: "Fast feedback loops",
+    description:
+      "ethui can skip annoying transaction reviews, allowing you to focus on what matters",
+  },
+  {
+    title: "Your own contract explorer",
+    description:
+      "By scanning compilation artifacts, we give you a UI to directly interact with your contracts",
+  },
+  {
+    title: "Multi wallet, multi browser, multi everything",
+    description:
+      "Setup multiple wallets without needing browser profiles; Connect websites to different chains simultaneously",
+  },
+];
+
+const footerLinks = [
+  { text: "Github", href: "https://github.com/ethui" },
+  { text: "Twitter", href: "https://twittter.com/ethuidev" },
+];
+
 function Home() {
   const [isHeaderVisible, setIsHeaderVisible] = useState(false);
   const heroRef = useRef<HTMLDivElement>(null);
@@ -25,7 +53,7 @@ function Home() {
       ([entry]) => {
         setIsHeaderVisible(!entry.isIntersecting);
       },
-      { root: null, rootMargin: "0px", threshold: 0.5 },
+      { root: null, rootMargin: "0px", threshold: 0.8 },
     );
 
     if (heroRef.current) {
@@ -38,11 +66,12 @@ function Home() {
       }
     };
   }, []);
-
   return (
     <>
       <Header hero isVisible={isHeaderVisible} />
       <Hero ref={heroRef} />
+      <Highlights />
+      <Footer />
     </>
   );
 }
@@ -154,5 +183,66 @@ function Hero({ ref }: { ref: Ref<HTMLDivElement> }) {
         </div>
       </div>
     </div>
+  );
+}
+
+export default function Highlights() {
+  return (
+    <section className="flex flex-col md:flex-row px-4">
+      <div className="sticky h-screen md:top-0 md:w-1/3 md:self-start flex flex-col gap-2">
+        <div className="flex flex-col gap-2 h-screen flex flex-col md:justify-center">
+          <h2 className=" font-bold text-3xl">Ethereum made easy</h2>
+          <p className="">
+            Developing for web3 can be challenging, we're here to help.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 md:w-2/3 md:gap-20">
+        {features.map((feature, index) => (
+          <div key={index} className="flex flex-col gap-2">
+            <h3 className=" font-semibold text-2xl">{feature.title}</h3>
+            <p className=" text-secondary-foreground">{feature.description}</p>
+            <img
+              src="https://i.ytimg.com/vi/NpEaa2P7qZI/maxresdefault.jpg"
+              alt="placeholder"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="p-6">
+      <div className="flex flex-col items-center justify-between md:flex-row">
+        <Link className="flex items-center justify-center" to="/">
+          <img
+            src="https://avatars.githubusercontent.com/u/164216877?s=200&v=4"
+            alt="ethui logo"
+            className="h-8 w-auto"
+          />
+          <span className="ml-2 font-bold text-2xl">ethui</span>
+        </Link>
+        <div>
+          <ul className="flex space-x-4">
+            {footerLinks.map(({ text, href }) => (
+              <li key={text}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-gray-300"
+                >
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
 }
