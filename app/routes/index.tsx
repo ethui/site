37610bf141d6251fb282c931466c306961c6ebba
Miguel Palhas <mpalhas@gmail.com>
2025-02-16 09:26:53 +0000

    feature highlights on landing page (#21)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,16 @@
 import { ButtonWithDropdown } from "@ethui/ui/components/button-with-dropdown";
 import { Button } from "@ethui/ui/components/shadcn/button";
 import { Link, createFileRoute } from "@tanstack/react-router";
-import { FileCode2 } from "lucide-react";
+import { ChevronDown, FileCode2 } from "lucide-react";
 import { type Ref, useEffect, useRef, useState } from "react";
 import { Header } from "#/components/header";
 import { AppleIcon, LinuxIcon } from "#/components/icons";
+import { SmoothScrollLink } from "#/components/smooth-scroll-link";
+
+import videoContracts from "#/assets/videos/ethui-contracts.webm?url";
+import videoFastMode from "#/assets/videos/ethui-fast-mode.webm?url";
+import videoLocal from "#/assets/videos/ethui-local.webm?url";
+import videoMulti from "#/assets/videos/ethui-multi.webm?url";
 
 export const Route = createFileRoute("/")({
   component: Home,
@@ -16,6 +22,38 @@ interface Data {
   linux: any;
 }
 
+const features = [
+  {
+    title: "Local-first",
+    description:
+      "ethui works directly with anvil and forge to provide a fast and reproducible environment",
+    video: videoLocal,
+  },
+  {
+    title: "Fast feedback loops",
+    description:
+      "ethui can skip annoying transaction reviews, allowing you to focus on what matters",
+    video: videoFastMode,
+  },
+  {
+    title: "Your own contract explorer",
+    description:
+      "By scanning compilation artifacts, we give you a UI to directly interact with your contracts",
+    video: videoContracts,
+  },
+  {
+    title: "Multi wallet, multi browser",
+    description:
+      "Setup multiple wallets without needing browser profiles; Connect websites to different chains simultaneously",
+    video: videoMulti,
+  },
+];
+
+const footerLinks = [
+  { text: "Github", href: "https://github.com/ethui" },
+  { text: "Twitter", href: "https://twittter.com/ethuidev" },
+];
+
 function Home() {
   const [isHeaderVisible, setIsHeaderVisible] = useState(false);
   const heroRef = useRef<HTMLDivElement>(null);
@@ -25,7 +63,7 @@ function Home() {
       ([entry]) => {
         setIsHeaderVisible(!entry.isIntersecting);
       },
-      { root: null, rootMargin: "0px", threshold: 0.5 },
+      { root: null, rootMargin: "0px", threshold: 0.8 },
     );
 
     if (heroRef.current) {
@@ -38,11 +76,12 @@ function Home() {
       }
     };
   }, []);
-
   return (
     <>
       <Header hero isVisible={isHeaderVisible} />
       <Hero ref={heroRef} />
+      <Highlights />
+      <Footer />
     </>
   );
 }
@@ -63,7 +102,6 @@ function Hero({ ref }: { ref: Ref<HTMLDivElement> }) {
       const linux = json.assets.find((asset: any) =>
         asset.name.includes("AppImage"),
       );
-      console.log(linux);
 
       setData({ version, osx, linux });
     })();
@@ -96,7 +134,11 @@ function Hero({ ref }: { ref: Ref<HTMLDivElement> }) {
   const defaultLink = isLinux ? linuxLink : macOsLink;
 
   return (
-    <div className="flex h-screen flex-col justify-center" ref={ref}>
+    <div
+      className="flex h-screen flex-col items-center justify-center"
+      ref={ref}
+      id="hero"
+    >
       <div className="isolate flex flex-col">
         <div className="flex items-center justify-center">
           <img
@@ -114,7 +156,7 @@ function Hero({ ref }: { ref: Ref<HTMLDivElement> }) {
           </div>
         </div>
 
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-between gap-x-6">
           <span>
             <ButtonWithDropdown
               asChild
@@ -152,7 +194,89 @@ function Hero({ ref }: { ref: Ref<HTMLDivElement> }) {
             </a>
           </Button>
         </div>
+
+        <SmoothScrollLink
+          className="mt-10 flex items-center justify-center gap-2 text-center"
+          hash="highlights"
+        >
+          <p>scroll for details</p>
+          <ChevronDown className="h-4 w-4 animate-bounce" />
+        </SmoothScrollLink>
       </div>
     </div>
+  );
+}
+
+function Highlights() {
+  return (
+    <section
+      className="flex w-full flex-col self-center md:flex-row"
+      id="highlights"
+    >
+      <div className="sticky flex flex-col bg-sidebar md:top-0 md:w-2/5 md:self-start">
+        <div className="flex flex-col gap-2 px-4 py-16 md:h-screen md:items-end md:justify-center md:px-8 md:py-0">
+          <h2 className=" font-bold text-3xl">Ethereum made easy</h2>
+          <p className="text-right">
+            Developing for web3 can be challenging, we're here to help.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2 px-4 py-8 md:w-3/5 md:gap-40 md:px-8">
+        {features.map(({ title, description, video }, index) => (
+          <div
+            key={index}
+            className="flex h-[60vh] flex-col justify-center gap-2"
+          >
+            <h3 className="font-semibold text-2xl">{title}</h3>
+            <p className="text-secondary-foreground">{description}</p>
+            {video && (
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="mr-4 aspect-16/9 max-w-3xl py-8"
+              >
+                <source src={video} type="video/webm" />
+              </video>
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="p-6">
+      <div className="flex flex-col items-center justify-between md:flex-row">
+        <Link className="flex items-center justify-center" to="/" hash="hero">
+          <img
+            src="https://avatars.githubusercontent.com/u/164216877?s=200&v=4"
+            alt="ethui logo"
+            className="h-8 w-auto"
+          />
+          <span className="ml-2 font-bold text-2xl">ethui</span>
+        </Link>
+        <div>
+          <ul className="flex space-x-4">
+            {footerLinks.map(({ text, href }) => (
+              <li key={text}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-gray-300"
+                >
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
   );
 }
---
+++ index.d.ts
@@ -0,0 +1,4 @@
+declare module "#/assets/videos/ethui-multi.webm?url";
+declare module "#/assets/videos/ethui-fast-mode.webm?url";
+declare module "#/assets/videos/ethui-contracts.webm?url";
+declare module "#/assets/videos/ethui-local.webm?url";
