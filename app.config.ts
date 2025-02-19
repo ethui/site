044c22283f7199fd4d8d5b1fc4dbc22054d9ea0d
Miguel Palhas <mpalhas@gmail.com>
2025-02-19 15:17:59 +0000

    Upgrade dependencies

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -5,6 +6,7 @@ export default defineConfig({
   server: { preset: "vercel" },
   vite: {
     plugins: [
+      tailwindcss(),
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
