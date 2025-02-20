29b248e5f5282ea9a606667cc56c0bde975079f4
Miguel Palhas <mpalhas@gmail.com>
2025-02-20 17:42:43 +0000

    wip

--- app/utils/loadDocs.ts
+++
@@ -1,5 +0,0 @@
-import { createServerFn } from "@tanstack/start";
-
-export const loadLods = createServerFn({ method: "GET" })
-  .validator(z.object({ filePath: z.string() }))
-  .handler(async ({ data: { filePath } }) => {});
