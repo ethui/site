42c578b4c1a184b5098bf5819df626cf3ba6ed80
Miguel Palhas <mpalhas@gmail.com>
2025-02-21 15:00:33 +0000

    wip

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -11,13 +11,13 @@ const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null // Render nothing in production
     : lazy(() =>
-      // Lazy load in development
-      import("@tanstack/router-devtools").then((res) => ({
-        default: res.TanStackRouterDevtools,
-        // For Embedded Mode
-        // default: res.TanStackRouterDevtoolsPanel
-      })),
-    );
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export interface RouteContext {
   breadcrumb?: string;
