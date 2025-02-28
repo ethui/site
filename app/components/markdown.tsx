98b516a8b870d40a3653e32de70125ab5e0dc0be
Miguel Palhas <mpalhas@gmail.com>
2025-02-28 19:07:03 +0000

    wip

---
+++ app/components/markdown.tsx
@@ -0,0 +1,40 @@
+import { MDXProvider } from "@mdx-js/react";
+import clsx from "clsx";
+
+export function Markdown({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="prose">
+      <MDXProvider components={{ img: Image, Youtube }}>{children}</MDXProvider>
+    </div>
+  );
+}
+
+function Image({
+  src,
+  alt,
+  className = "",
+  ...props
+}: React.ComponentProps<"img">) {
+  return (
+    <img
+      className={clsx("mx-auto", className)}
+      {...props}
+      src={src}
+      alt={"asd"}
+    />
+  );
+}
+
+function Youtube({ id }: { id: string }) {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src={`https://www.youtube.com/embed/${id}`}
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
+    />
+  );
+}
