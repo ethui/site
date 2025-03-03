4a804f872dc98fe4be8a13be735a45eae5b1ef53
Miguel Palhas <mpalhas@gmail.com>
2025-03-03 16:36:07 +0000

    wip

--- app/server/embed.ts
+++ app/server/embed.ts
@@ -1,41 +1,48 @@
 import { createServerFn } from "@tanstack/start";
 import { load } from "cheerio";
 import { z } from "zod";
+import { fetchCached } from "./cache";
+
+const oneWeek = 1000 * 60 * 60 * 24 * 7;
 
 export const getOpengraphEmbedData = createServerFn({ method: "GET" })
   .validator(z.object({ url: z.string().url() }))
   .handler(async (ctx) => {
-    const url = URL.parse(ctx.data.url);
+    return await fetchCached({
+      key: `embed-${ctx.data.url}`,
+      ttl: oneWeek,
+      fn: async () => {
+        const url = URL.parse(ctx.data.url);
 
-    if (!url) {
-      throw new Error("Invalid URL");
-    }
+        if (!url) {
+          throw new Error("Invalid URL");
+        }
 
-    const response = await fetch(url.toString());
-    if (!response.ok) {
-      throw new Error(`Failed to fetch github embed data for ${url}`);
-    }
+        const response = await fetch(url.toString());
+        if (!response.ok) {
+          throw new Error(`Failed to fetch github embed data for ${url}`);
+        }
 
-    const text = await response.text();
+        const text = await response.text();
 
-    const $ = load(text);
-    const image = $('meta[property="og:image"]').attr("content") || "";
-    const title = $('meta[property="og:title"]').attr("content") || "";
-    const description = $("body").attr("content") || "";
-    console.log("desc", $("head").html());
-    console.log(title, description);
+        const $ = load(text);
+        const image = $('meta[property="og:image"]').attr("content") || "";
+        const title = $('meta[property="og:title"]').attr("content") || "";
+        const description = $("body").attr("content") || "";
 
-    return {
-      image,
-      title,
-      description,
-      url: {
-        pathname: url.pathname,
-        search: url.search,
-        hash: url.hash,
-        host: url.host,
+        return {
+          image,
+          title,
+          description,
+          url: {
+            pathname: url.pathname,
+            search: url.search,
+            hash: url.hash,
+            host: url.host,
+          },
+        };
       },
-    };
+    });
   });
 
 export type GithubMetadata = Awaited<ReturnType<typeof getOpengraphEmbedData>>;
