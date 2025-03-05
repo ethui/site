1032f41c082c862df33a3ba20f1da3d9bd42dec9
Miguel Palhas <mpalhas@gmail.com>
2025-03-05 17:45:28 +0000

    wip

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -29,105 +29,52 @@ function Image({
   className = "",
   ...props
 }: React.ComponentProps<"img">) {
-  return (
-    <img
-      {...props}
-      className={clsx("mx-auto", className)}
-      src={src}
-      alt={"asd"}
-    />
-  );
+  return "Image";
+  //return (
+  //  <img
+  //    {...props}
+  //    className={clsx("mx-auto", className)}
+  //    src={src}
+  //    alt={"asd"}
+  //  />
+  //);
 }
 
 function Video({ src }: { src: string }) {
-  return (
-    <picture className="flex w-full justify-center">
-      <video muted autoPlay loop controls src={src} className="max-w-[600px]" />
-    </picture>
-  );
+  return "Video";
+  //return (
+  //  <picture className="flex w-full justify-center">
+  //    <video muted autoPlay loop controls src={src} className="max-w-[600px]" />
+  //  </picture>
+  //);
 }
 
 function Youtube({ id }: { id: string }) {
-  return (
-    <iframe
-      width="560"
-      height="315"
-      src={`https://www.youtube.com/embed/${id}`}
-      title="YouTube video player"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-      className="mx-auto"
-    />
-  );
+  return "Youtube";
+  //return (
+  //  <iframe
+  //    width="560"
+  //    height="315"
+  //    src={`https://www.youtube.com/embed/${id}`}
+  //    title="YouTube video player"
+  //    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  //    referrerPolicy="strict-origin-when-cross-origin"
+  //    allowFullScreen
+  //    className="mx-auto"
+  //  />
+  //);
 }
 
 function SuspendedEmbed({ url }: { url: string }) {
-  return (
-    <a
-      href={url}
-      rel="noopener noreferrer"
-      target="_blank"
-      className="not-prose my-8 block overflow-hidden rounded-xl border-1 bg-background no-underline hover:bg-accent md:h-[9rem]"
-    >
-      <NonSuspenseEmbed url={url} />
-    </a>
-  );
-}
-
-// Keeping this for reference
-function Embed({ url: urlStr }: { url: string }) {
-  return "Not used";
-}
-
-// New implementation without Suspense
-
-function NonSuspenseEmbed({ url: urlStr }: { url: string }) {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["github-embed", urlStr],
-    queryFn: () => getOpengraphEmbedData({ data: { url: urlStr } }),
-  });
-  
-  if (isLoading) {
-    return (
-      <div className="flex h-full w-full items-center justify-center gap-2 border">
-        <LoaderCircle className="animate-spin" />
-        {urlStr}
-      </div>
-    );
-  }
-  
-  if (isError || !data) {
-    return (
-      <div className="flex flex-col items-stretch p-4">
-        <h1 className="font-bold text-base">External Link</h1>
-        <p className="flex items-center gap-2">
-          <LinkIcon size="14" />
-          {urlStr}
-        </p>
-      </div>
-    );
-  }
-  
-  const { image, title, description, url } = data;
-  
-  return (
-    <div className="flex flex-col items-stretch md:flex-row-reverse">
-      <div className="aspect-16/9 shrink-0 md:w-[16rem]">
-        <img
-          src={image}
-          className="mt-0 mb-0 h-full object-cover"
-          alt={title}
-        />
-      </div>
-      <div className="hidden flex-col items-stretch justify-between gap-2 overflow-hidden p-2 md:flex grow">
-        <h1 className="font-bold text-base">{title}</h1>
-        <p className="line-clamp-3 font-light text-sm">{description}</p>
-        <p className="flex items-center gap-2">
-          <LinkIcon size="14" />
-          {url.host}
-        </p>
-      </div>
-    </div>
-  );
+  return "SuspendedEmbed";
+//return (
+//  <a
+//    href={url}
+//    rel="noopener noreferrer"
+//    target="_blank"
+//    className="not-prose my-8 block overflow-hidden rounded-xl border-1 bg-background no-underline hover:bg-accent md:h-[9rem]"
+//  >
+//    <NonSuspenseEmbed url={url} />
+//  </a>
+//);
 }
