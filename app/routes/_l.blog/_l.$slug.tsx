34dbaf0f09bb7f36480c5f02b7aebb13a02158ee
Miguel Palhas <mpalhas@gmail.com>
2025-03-05 18:00:36 +0000

    Revert "wip"
    This reverts commit fcd01f65c207f3d9f715d5459204d1874f59ed00.

---
+++ app/routes/_l.blog/_l.$slug.tsx
@@ -0,0 +1,25 @@
+import { NotFound } from "#/components/NotFound";
+import { createFileRoute } from "@tanstack/react-router";
+import { blogManifest } from "./-manifest";
+import { Markdown } from "#/components/markdown";
+
+export const Route = createFileRoute("/_l/blog/_l/$slug")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { slug } = Route.useParams();
+  const post = blogManifest.find(
+    ({ frontmatter }) => frontmatter.slug === slug,
+  );
+
+  if (!post) {
+    return <NotFound />;
+  }
+
+  return (
+    <Markdown className="mb-16">
+      <post.default />
+    </Markdown>
+  );
+}
---
+++ app/routes/_l.blog/_l.index.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { blogManifest } from "./-manifest";
+
+export const Route = createFileRoute("/_l/blog/_l/")({
+  loader: () =>
+    blogManifest
+      .slice()
+      .reverse()
+      .map(({ frontmatter }) => frontmatter),
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const posts = Route.useLoaderData();
+
+  return (
+    <ul className="prose">
+      {posts.map(({ title, slug }) => (
+        <li key={slug} className="border-b pb-2">
+          <Link
+            to="/blog/$slug"
+            params={{ slug }}
+            className="font-medium text-lg no-underline transition-colors hover:text-primary"
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
