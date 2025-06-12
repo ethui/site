f07a7ce221d13071cf2fe590b024579b0a294d5f
Miguel Palhas <mpalhas@gmail.com>
2025-06-12 22:25:42 +0100

    tanstack/start breaking changes (#105)

---
+++ src/routes/_l.blog/-manifest.ts
@@ -0,0 +1,27 @@
+import * as blog01 from "#/blog/01-announcing-ethui/index.mdx";
+import * as blog02 from "#/blog/02-ethui-0.2.0-ui-level-up/index.mdx";
+import * as blog03 from "#/blog/03-ethui-0.3.1-mainnet/index.mdx";
+import * as blog04 from "#/blog/04-instant-foundry-abi-explorer/index.mdx";
+import * as blog05 from "#/blog/05-ethui-0.5.0-impersonation/index.mdx";
+import * as blog06 from "#/blog/06-ethui-0.6.0-going-multi-chain/index.mdx";
+import * as blog07 from "#/blog/07-ethui-0.7.0-eth-lisbon/index.mdx";
+import * as blog08 from "#/blog/08-ethui-1.1.1-ledger-devtools/index.mdx";
+import * as blog09 from "#/blog/09-ethui-1.6-a-new-beginning/index.mdx";
+import * as blog10 from "#/blog/10-ethui-1.7-forms/index.mdx";
+import * as blog11 from "#/blog/11-ethui-1.13-were-back/index.mdx";
+import * as blog12 from "#/blog/12-ethui-1.14/index.mdx";
+
+export const blogManifest = [
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+  blog06,
+  blog07,
+  blog08,
+  blog09,
+  blog10,
+  blog11,
+  blog12,
+];
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import mdx from "@mdx-js/rollup";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeAddClasses from "rehype-class-names";
+import rehypeExternalLinks from "rehype-external-links";
+import { rehypeGithubAlerts } from "rehype-github-alerts";
+import rehypeGithubEmoji from "rehype-github-emoji";
+import rehypeMdxImportMedia from "rehype-mdx-import-media";
+import rehypeSlug from "rehype-slug";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+const classes = {
+  "h1>a, h2>a, h3>a": "no-underline font-bold",
+};
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    mdx({
+      providerImportSource: "@mdx-js/react",
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: "wrap" }],
+
+        [rehypeAddClasses, classes],
+        [rehypeExternalLinks, { target: "_blank", rel: "noopener noreferrer" }],
+        rehypeGithubAlerts,
+        rehypeGithubEmoji,
+        rehypeMdxImportMedia,
+      ],
+    }),
+    tanstackStart({
+      target: "vercel",
+    }),
+    tailwindcss(),
+  ],
+});
