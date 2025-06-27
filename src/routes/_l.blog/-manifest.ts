46317b557298c72b5d18db5c99aa2921673f34a2
Miguel Palhas <mpalhas@gmail.com>
2025-06-27 16:05:32 +0100

    wip

--- src/routes/_l.blog/-manifest.ts
+++ src/routes/_l.blog/-manifest.ts
@@ -10,6 +10,7 @@ import * as blog09 from "#/blog/09-ethui-1.6-a-new-beginning/index.mdx";
 import * as blog10 from "#/blog/10-ethui-1.7-forms/index.mdx";
 import * as blog11 from "#/blog/11-ethui-1.13-were-back/index.mdx";
 import * as blog12 from "#/blog/12-ethui-1.14/index.mdx";
+import * as blog13 from "#/blog/13-ethui-explorer/index.mdx";
 
 export const blogManifest = [
   blog01,
@@ -24,4 +25,5 @@ export const blogManifest = [
   blog10,
   blog11,
   blog12,
+  blog13,
 ];
