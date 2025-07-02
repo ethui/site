58a17ab6cd12ae1a4f02aa6645386677b7cdfd20
Miguel Palhas <mpalhas@gmail.com>
2025-07-02 18:00:15 +0100

    wip

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -98,16 +98,14 @@ function RootDocument({ children }: { children: React.ReactNode }) {
               async
               src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
             />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${googleAnalyticsId}');
-                `,
-              }}
-            />
+            <script>
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${googleAnalyticsId}');
+              `}
+            </script>
           </>
         )}
       </head>
