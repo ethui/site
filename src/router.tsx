a4b921c1357fd35eed6b677ef2bf9601d97b4001
Miguel Palhas <mpalhas@gmail.com>
2025-09-26 17:48:40 +0100

    wip

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,17 @@
-import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { createRouter as createTanstackRouter } from "@tanstack/react-router";
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
 import { NotFound } from "./components/NotFound";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
-  const router = createTanStackRouter({
+  createTanstackRouter({
     routeTree,
     defaultPreload: "intent",
     defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
     scrollRestoration: true,
-  });
+  })
 
-  return router;
 }
 
 declare module "@tanstack/react-router" {
