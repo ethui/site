6fd3f1fa1387f41c6de47a343b842428b4939576
Miguel Palhas <mpalhas@gmail.com>
2025-09-27 08:29:53 +0100

    Fixing tanstack (and migrating to pnpm)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeAddClasses from "rehype-class-names";
 import rehypeExternalLinks from "rehype-external-links";
@@ -36,9 +38,11 @@ export default defineConfig({
         rehypeMdxImportMedia,
       ],
     }),
-    tanstackStart({
-      target: "vercel",
+    tanstackStart(),
+    nitroV2Plugin({
+      compatibilityDate: "2025-09-25",
     }),
+    viteReact(),
     tailwindcss(),
   ],
 });
