89fc817890c0ce387557722ad3644a98e8e8c6e0
Miguel Palhas <mpalhas@gmail.com>
2026-01-30 18:18:47 +0000

    wip

--- src/routes/_l.blog/-manifest.ts
+++ src/routes/_l.blog/-manifest.ts
@@ -39,7 +39,7 @@ const getDirectoryFromSlug = (slug: string): string => {
     "ethui-1.14.0": "12-ethui-1.14",
     "ethui-explorer": "13-ethui-explorer",
     stacks: "14-ethui-stacks",
-    "ethui-1.27.0-hardhat-support": "15-ethui-1.27-hardhat-support",
+    "ethui-1.27-hardhat-support": "15-ethui-1.27-hardhat-support",
   };
   return slugToDir[slug] || slug;
 };
